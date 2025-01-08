ed419d6fce5f5b3ad22b855899de00d0ede43a73
Favio-Coder <faviusam@gmail.com>
2025-01-07 20:01:42 -0500

    Arreglando login v2

--- src/app/login/component/LoginForm.jsx
+++ src/app/login/component/LoginForm.jsx
@@ -72,7 +72,7 @@ export default function LoginForm() {
     };
 
     return (
-        <div>
+        <div className="mx-auto my-auto w-screen h-screen items-center flex-col overflow-y-auto overflow-x-hidden lg:overflow-hidden">
             <header>
                 <div className="text-colorHover flex items-center justify-between w-full px-4 my-6 sm:px-8">
                     <Link href="/" className="w-35 text-colorHover">
@@ -82,20 +82,19 @@ export default function LoginForm() {
                     </Link>
                     <a href="">
                         <p className="text-2xl">Alzhivida</p>
-
                     </a>
                     <span className="w-35"></span>
                 </div>
             </header>
-
-            <div className="overflow-hidden">
-                <div className="flex  container mx-auto my-auto w-screen h-screen items-center flex-col">
+    
+            <div>
+                <div className="flex container mx-auto my-auto w-screen h-screen items-center flex-col">
                     <div className="text-colorHover items-center">
                         <div className="text-center pb-3">
                             <p className="text-[3rem]">!Bienvenido!</p>
                         </div>
                     </div>
-
+    
                     <div className="w-full md:w-3/4 lg:w-1/2 flex flex-col items-center bg-slate-100 rounded-md pt-12 shadow-xl">
                         <form onSubmit={enviarFormulario} className="w-3/4">
                             <div className="mb-6">
@@ -107,7 +106,6 @@ export default function LoginForm() {
                                     placeholder="Correo electrónico"
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
-
                                 />
                                 <Alert
                                     isMessageAlert={isAlertVisibleEmail}
@@ -115,8 +113,7 @@ export default function LoginForm() {
                                     messageAlert="Debe rellenar este campo"
                                 />
                             </div>
-
-
+    
                             <div className="mb-6">
                                 <div className="flex items-center relative z-50">
                                     <input
@@ -127,9 +124,8 @@ export default function LoginForm() {
                                         placeholder="Contraseña"
                                         value={password}
                                         onChange={(e) => setPassword(e.target.value)}
-
                                     />
-
+    
                                     <span
                                         className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
                                         onClick={mostrarContraseña}
@@ -144,9 +140,7 @@ export default function LoginForm() {
                                     messageAlert="Debe rellenar este campo"
                                 />
                             </div>
-
-
-
+    
                             <div className="mb-12">
                                 <button type="submit" className="py-4 bg-colorHover w-full rounded text-blue-50 font-bold hover:shadow-md">Ingresar</button>
                                 <p className="text-center">o continua con</p>
@@ -157,20 +151,17 @@ export default function LoginForm() {
                                             <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
                                         </svg>
                                     </div>
-
                                 </button>
                             </div>
                         </form>
                     </div>
-
+    
                     <div className="flex justify-center container mx-auto mt-6 text-colorHover text-sm">
                         <div className="flex flex-col sm:flex-row justify-between md:w-1/2 items-center">
-
                             <Link href="">
                                 <div className="flex hover:underline">¿Olvidaste la contraseña?</div>
                             </Link>
-
-
+    
                             <Link href="">
                                 <div className="flex gap-1">
                                     <span className="text-black"> ¿No tienes una cuenta?  </span>
@@ -183,4 +174,5 @@ export default function LoginForm() {
             </div>
         </div>
     );
+    
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,9 +4,10 @@ export default function loginPage() {
     return (
         //Cuando no se puede hacer Scrool, es que la pantalla no esta ocupando por completo 
         //Usar w-scree y h-screen
-        <div className="bg-color-gray overflow-hidden w-screen h-screen md:overflow-auto">
+        <div className="bg-color-gray w-screen h-screen overflow-hidden lg:overflow-hidden lg:h-auto">
             <LoginForm />
         </div>
+
 
     )
 }
